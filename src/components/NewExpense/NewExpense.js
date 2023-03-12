f7ddb895cca6b64faace70f24d4a3d603c470602
uchexm <109084069+uchexm@users.noreply.github.com>
2023-03-12 12:35:38 +0200

    props

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expensedata = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expensedata);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+export default NewExpense;
